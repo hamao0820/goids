065c1ccd67a04fc3524e606e4e99dd7e0a7a4e2d
浜田　俊祐 <hamashun0820@icloud.com>
2023-10-30 14:01:54 +0900

    feat: メソッドを追加

--- vector.go
+++ vector.go
@@ -1,5 +1,7 @@
 package main
 
+import "math"
+
 type Vector struct {
 	X float64
 	Y float64
@@ -7,4 +9,17 @@ type Vector struct {
 
 func CreateVector(x, y float64) Vector {
 	return Vector{X: x, Y: y}
+}
+
+func (v Vector) Len() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func (v *Vector) ScalarMul(c float64) {
+	v.X *= c
+	v.Y *= c
+}
+
+func (v *Vector) Scale(l float64) {
+	v.ScalarMul(l / v.Len())
 }
