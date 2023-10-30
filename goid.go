5370ead1625e19ad66f11467345f6a7dc9b0e5b2
浜田　俊祐 <hamashun0820@icloud.com>
2023-10-30 17:53:14 +0900

    feat: Seekを実装

--- goid.go
+++ goid.go
@@ -7,3 +7,10 @@ type Goid struct {
 	maxSpeed     float64
 	maxForce     float64
 }
+
+func (g *Goid) Seek(t Vector) {
+	tv := Sub(t, g.position)
+	tv.Limit(g.maxSpeed)
+	force := Sub(tv, g.velocity)
+	g.acceleration.Add(force)
+}
