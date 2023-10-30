652d1dd838599e365af9aa7260cd62f14445c6ae
浜田　俊祐 <hamashun0820@icloud.com>
2023-10-30 18:02:44 +0900

    feat: Alignを実装

--- goid.go
+++ goid.go
@@ -26,3 +26,20 @@ func (g Goid) IsInsight(g2 Goid) bool {
 	d := Sub(g.position, g2.position).Len()
 	return d < 100
 }
+
+func (g Goid) Align(goids []Goid) {
+	var avgVel Vector
+	n := 0
+	for _, other := range goids {
+		if g == other || !g.IsInsight(other) {
+			continue
+		}
+		avgVel.Add(other.velocity)
+		n++
+	}
+	if n > 0 {
+		avgVel.ScalarMul(1 / float64(n))
+		avgVel.Limit(g.maxSpeed)
+		g.acceleration.Add(Sub(avgVel, g.velocity))
+	}
+}
