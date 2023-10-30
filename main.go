c1ed61a853828ec82cc0019c98c6c0644b5a673f
浜田　俊祐 <hamashun0820@icloud.com>
2023-10-30 19:20:04 +0900

    feat: ターミナルに表示できるようにした

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
 import (
+	"bytes"
+	"encoding/base64"
+	"fmt"
+	"image"
+	"image/color"
+	"image/png"
+	"math"
 	"math/rand"
+
+	"github.com/llgcode/draw2d/draw2dimg"
 )
 
 func main() {
@@ -16,6 +25,8 @@ func main() {
 		maxForce := 2
 		goids[i] = Goid{position: position, velocity: velocity, maxSpeed: float64(maxSpeed), maxForce: float64(maxForce)}
 	}
+	clearScreen()
+	hideCursor()
 
 	for i := 0; i < 1000; i++ {
 		for i := 0; i < len(goids); i++ {
@@ -23,5 +34,45 @@ func main() {
 			goid.Flock(goids)
 			goid.Update(width, height)
 		}
+		frame := draw(goids)
+		fmt.Printf("loop: %d\n", i)
+		printImage(frame.SubImage(frame.Rect))
 	}
+	showCursor()
+}
+
+// draw the goids
+func draw(goids []Goid) *image.RGBA {
+	dest := image.NewRGBA(image.Rect(0, 0, 640, 480))
+	gc := draw2dimg.NewGraphicContext(dest)
+	for _, goid := range goids {
+		gc.SetFillColor(color.RGBA{200, 200, 100, 255})
+		gc.MoveTo(float64(goid.position.X), float64(goid.position.Y))
+		gc.ArcTo(float64(goid.position.X), float64(goid.position.Y), float64(5), float64(5), 0, -math.Pi*2)
+		// gc.LineTo(float64(goid.X-goid.Vx), float64(goid.Y-goid.Vy))
+		gc.Close()
+		gc.Fill()
+	}
+	return dest
+}
+
+// ANSI escape sequence codes to perform action on terminal
+func hideCursor() {
+	fmt.Print("\033[?25l")
+}
+
+func showCursor() {
+	fmt.Print("\x1b[?25h\n")
+}
+
+func clearScreen() {
+	fmt.Print("\x1b[2J")
+}
+
+// this only works for iTerm!
+func printImage(img image.Image) {
+	var buf bytes.Buffer
+	png.Encode(&buf, img)
+	imgBase64Str := base64.StdEncoding.EncodeToString(buf.Bytes())
+	fmt.Printf("\x1b[2;0H\x1b]1337;File=inline=1:%s\a", imgBase64Str)
 }
