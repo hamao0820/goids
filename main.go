233118122076bf0b5bc4f4331a78da22696b558c
浜田　俊祐 <hamashun0820@icloud.com>
2023-11-01 10:23:12 +0900

    feat: 初期状態の画像を出力できるようにした

--- main.go
+++ main.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"image"
-	"image/draw"
-	"math/rand"
-	"os"
-	"time"
+	"goids/goids"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -13,43 +9,18 @@ import (
 )
 
 func main() {
-	width, height := 100, 100
+	width, height := 640, 480
 
 	myApp := app.New()
 	window := myApp.NewWindow("Image")
 	window.Resize(fyne.NewSize(float32(width), float32(height)))
 
+	e := goids.CreateEnv(float64(width), float64(height), 30, 4, 2)
+
 	go func() {
-		f, err := os.Open("test.png")
-		if err != nil {
-			panic(err)
-		}
-		defer f.Close()
-		img, _, err := image.Decode(f)
-		if err != nil {
-			panic(err)
-		}
-		dest := image.NewRGBA(image.Rect(0, 0, width, height))
-		p := image.Point{rand.Intn(width), rand.Intn(height)}
-		rectAngle := image.Rectangle{p.Sub(img.Bounds().Size().Div(2)), p.Add(img.Bounds().Size().Div(2))}
-		draw.Draw(dest, rectAngle, img, image.Point{0, 0}, draw.Over)
-
-		img_ := dest.SubImage(dest.Rect)
-		imageWidget := canvas.NewImageFromImage(img_)
-		imageWidget.Image = img_
+		img := e.RenderImage()
+		imageWidget := canvas.NewImageFromImage(img)
 		window.SetContent(imageWidget)
-		for {
-			dest := image.NewRGBA(image.Rect(0, 0, width, height))
-			p := image.Point{rand.Intn(width), rand.Intn(height)}
-			rectAngle := image.Rectangle{p.Sub(img.Bounds().Size().Div(2)), p.Add(img.Bounds().Size().Div(2))}
-			draw.Draw(dest, rectAngle, img, image.Point{0, 0}, draw.Over)
-
-			img_ := dest.SubImage(dest.Rect)
-			imageWidget.Image = img_
-			imageWidget.Refresh()
-
-			time.Sleep(100 * time.Millisecond)
-		}
 	}()
 
 	window.ShowAndRun()
