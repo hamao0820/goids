a99a4693d977564e75952f0f3d9c41f3b353d6af
浜田　俊祐 <hamashun0820@icloud.com>
2023-11-01 14:19:08 +0900

    fix: 引数を使うように修正

--- goids/env.go
+++ goids/env.go
@@ -28,8 +28,8 @@ func CreateEnv(width, height float64, n int, maxSpeed, maxForce float64) Environ
 		position := CreateVector(rand.Float64()*width, rand.Float64()*height)
 		velocity := CreateVector(rand.Float64()*2-1, rand.Float64()*2-1)
 		velocity.Scale(rand.Float64()*4 - rand.Float64()*2)
-		maxSpeed := 4
-		maxForce := 2
+		maxSpeed := maxSpeed
+		maxForce := maxForce
 		goids[i] = Goid{position: position, velocity: velocity, maxSpeed: float64(maxSpeed), maxForce: float64(maxForce)}
 	}
 
